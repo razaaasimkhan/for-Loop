b358984fa26b17f945321414e78027474faa6021
Aasim Raza <135644168+razaaasimkhan@users.noreply.github.com>
2024-07-31 20:25:05 +0530

    Add files via upload

---
+++ For.py
@@ -0,0 +1,12 @@
+def prime(num):
+    count = 0
+    for i in range(1, num + 1):
+        if num % i == 0:
+            count += 1
+    if count == 2:
+        return number
+
+
+for number in range(1, 50 + 1):
+    if prime(number):
+        print(number)
