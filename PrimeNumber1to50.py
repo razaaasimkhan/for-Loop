21f186218519c6c212ffe258cc234fcbfa91156f
Aasim Raza <135644168+razaaasimkhan@users.noreply.github.com>
2024-07-31 20:42:30 +0530

    Update PrimeNumber1to50.py

--- PrimeNumber1to50.py
+++ PrimeNumber1to50.py
@@ -10,3 +10,12 @@ def prime(num):
 for number in range(1, 50 + 1): #prime number 1 to 50
     if prime(number):
         print(number)
+
+#2nd method to find the prime number b/w 1 to 50
+for n in range(1, 50 + 1):
+    count = 0
+    for i in range(1, n + 1):
+        if n % i == 0:
+            count += 1
+    if count == 2:
+        print(n)
